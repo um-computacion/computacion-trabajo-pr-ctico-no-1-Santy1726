1d022b459774034dbe6c96099568c3d63fe84837
Santiago Ortega <sa.ortega@alumno.um.edu.ar>
2025-04-06 13:35:18 -0300

    fallo en la funcion de los test por falta de codigo

--- tests/test_romanos.py
+++ tests/test_romanos.py
@@ -2,8 +2,6 @@ import unittest
 from src.convertir_romano import convertir_a_romano 
 
 
-
-
 class PruebasConversionRomana(unittest.TestCase):
 
     def test_numeros_unidades(self):
@@ -11,6 +9,19 @@ class PruebasConversionRomana(unittest.TestCase):
         self.assertEqual(convertir_a_romano(5), "V")
         self.assertEqual(convertir_a_romano(8), "VIII")
 
+    def test_numeros_unidades(self):
+        self.asserEqual(convertir_a_romano(9), "IX")
+        self.assertEqual(convertir_a_romano(40), "XL")
+        self.assertEqual(convertir_a_romano(90), "XC")
+        self.assertEqual(convertir_a_romano(400), "CD")
+        self.assertEqual(convertir_a_romano(900), "CM")
+
+    def test_valores_complejos(self):
+        self.assertEqual(convertir_a_romano(1987), "MCMLXXXVII")
+        self.assertEqual(convertir_a_romano(2421), "MMCDXXI")
+        self.assertEqual(convertir_a_romano(3210), "MMMCCX")
+        self.assertEqual(convertir_a_romano(3999), "MMMCMXCIX")
+    
 if __name__ == '__main__':
 
     unittest.main()
